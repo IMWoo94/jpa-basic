4eeb6ddee98585f7387d7c12fbc2103650ed4b7b
IMWoo94 <leemeawoo94@gmail.com>
2023-10-13 20:56:05 +0900

    jpa basic JPQL 기본 문법 2023.10.13

--- jpql/src/main/java/hellojpa/Main.java
+++ jpql/src/main/java/hellojpa/Main.java
@@ -4,6 +4,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import hellojpa.jpql.Member;
 
 public class Main {
 	public static void main(String[] args) {
@@ -18,6 +22,29 @@ public class Main {
 		tx.begin();
 		try {
 
+			Member member = new Member();
+			member.setUsername("test");
+			em.persist(member);
+
+			// TypeQuery 반환 타입이 명확
+			TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+			TypedQuery<String> query1 = em.createQuery("select m.username from Member m", String.class);
+
+			// Query 반환 타입이 명확하지 않을 때 사용
+			Query query2 = em.createQuery("select m.username, m.age from Member m");
+
+			// getResultList -> 결과 List 형식으로 제공 단, 결과값 없으면 빈 리스트 반환
+			// 파라미터 바인딩 - 이름 기준
+			TypedQuery<Member> query3 = em.createQuery("select m from Member m where m.username =:username",
+				Member.class).setParameter("username", "empty");
+			System.out.println("getResultList -> 빈 리스트 확인 " + query3.getResultList().isEmpty());
+			// System.out.println("getSingleResult -> 결과가 무조건 1개 아니면 오류 발생 " + query.getSingleResult());
+			// System.out.println("getSingleResult -> 결과가 없어도 오류 발생 " + query3.getSingleResult());
+
+			// 파라미터 바인딩 - 위치기준
+			TypedQuery<Member> query4 = em.createQuery("select m from Member m where m.username =?0",
+				Member.class).setParameter(0, member.getUsername());
+			query4.getSingleResult();
 			tx.commit();
 
 		} catch (Exception e) {
